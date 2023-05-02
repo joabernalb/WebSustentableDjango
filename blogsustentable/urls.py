07b3cf26a9115034cf150be3f2e1b8589802946e
MiguelRedl <111456583+MiguelRedl@users.noreply.github.com>
2023-05-02 16:58:10 -0400

    subida imagenes nuevas agregado services a urls

--- blogsustentable/urls.py
+++ blogsustentable/urls.py
@@ -7,5 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
-    path('service1.html', views.service, name='Service1'),
+    path('service1.html', views.service1, name='service1'),
+    path('service2.html', views.service2, name='service2'),
+    path('service3.html', views.service3, name='service3'),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blogsustentable/views.py
+++ blogsustentable/views.py
@@ -5,11 +5,17 @@ from .models import Post
 IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
 
 
-def service(request):
-    return render(request, 'blog/service1.html', {})
-
 def index(request):
     return render(request, 'blog/index.html', {})
+
+def service1(request):
+    return render(request, 'blog/service1.html', {})
+
+def service2(request):
+    return render(request, 'blog/service2.html', {})
+
+def service3(request):
+    return render(request, 'blog/service3.html', {})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
