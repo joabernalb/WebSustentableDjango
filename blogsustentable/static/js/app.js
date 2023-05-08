2be1ddc5af487e06b1ff6a63c9e8bd46b0169af3
joabernalb <joa.bernal@duocuc.cl>
2023-05-08 16:18:54 -0400

    A

--- blogsustentable/static/js/app.js
+++ blogsustentable/static/js/app.js
@@ -27,3 +27,24 @@ window.addEventListener('load', function(){
 		]
 	});
 });
+
+
+function toggleResetPswd(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle()
+    $('#logreg-forms .form-reset').toggle() 
+}
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); 
+    $('#logreg-forms .form-signup').toggle(); 
+}
+
+$(()=>{
+    // Login Registrar
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
