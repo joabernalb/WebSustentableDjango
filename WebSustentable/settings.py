4e0d3349fc2b3de9ae8967b9bd567c94442eec30
joabernalb <joa.bernal@duocuc.cl>
2023-05-10 16:22:48 -0400

    ORDEN EN TRIOS

--- WebSustentable/settings.py
+++ WebSustentable/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogsustentable.apps.BlogsustentableConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
